90436764d05cdd95381e094d8702145afdfda749
santani423 <santani423@gmail.com>
2025-05-23 22:00:29 +0700

    simpan

--- app/Http/Controllers/Service/UjianServiceController.php
+++ app/Http/Controllers/Service/UjianServiceController.php
@@ -132,31 +132,31 @@ class UjianServiceController extends Controller
 
             $waktuUjian->update($dataEndTime);
         } else {
-            $mergeUjian = MergeUjian::join('relasi_ujian_merge as rum', 'rum.kode_merge_ujian', 'merge_ujian.kode')
-                ->join('ujian', 'ujian.kode', '=', 'rum.kode_ujian')
-                ->join('waktu_ujian', 'waktu_ujian.kode', '=', 'ujian.kode')
-                ->where('merge_ujian.kode', $kodeMergeUjian)
-                ->where('waktu_ujian.selesai', 1)
-                ->where('waktu_ujian.siswa_id', session()->get('id'))
-                ->select(
-                    'rum.*',
-                    'waktu_ujian.*',
-                    'ujian.jenis as jenis_ujian',
-                    'ujian.kode as kode_ujian',
-                    'merge_ujian.jam',
-                    'merge_ujian.menit',
-                    'merge_ujian.kode as merge_ujian_kode'
-                )
-                ->distinct('rum.id')
-                ->orderBy('rum.urutan', 'asc')
-                ->take(2) // ambil 2 data
-                ->get();
-
-            if ($mergeUjian) {
-                if ($mergeUjian[0]->kode_ujian != $kodeUjian) {
-                    
-                }
-            }
+            // $mergeUjian = MergeUjian::join('relasi_ujian_merge as rum', 'rum.kode_merge_ujian', 'merge_ujian.kode')
+            //     ->join('ujian', 'ujian.kode', '=', 'rum.kode_ujian')
+            //     ->join('waktu_ujian', 'waktu_ujian.kode', '=', 'ujian.kode')
+            //     ->where('merge_ujian.kode', $kodeMergeUjian)
+            //     ->where('waktu_ujian.selesai', 1)
+            //     ->where('waktu_ujian.siswa_id', session()->get('id'))
+            //     ->select(
+            //         'rum.*',
+            //         'waktu_ujian.*',
+            //         'ujian.jenis as jenis_ujian',
+            //         'ujian.kode as kode_ujian',
+            //         'merge_ujian.jam',
+            //         'merge_ujian.menit',
+            //         'merge_ujian.kode as merge_ujian_kode'
+            //     )
+            //     ->distinct('rum.id')
+            //     ->orderBy('rum.urutan', 'asc')
+            //     ->take(2) // ambil 2 data
+            //     ->get();
+            //         dd($mergeUjian);
+            // if ($mergeUjian) {
+            //     if ($mergeUjian[0]->kode_ujian != $kodeUjian) {
+            //         dd($mergeUjian[0]);
+            //     }
+            // }
         }
 
         $ujian = Ujian::where('kode', $kodeUjian)->first();
