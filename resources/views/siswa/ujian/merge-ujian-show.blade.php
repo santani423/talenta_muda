60b4abd6c470056b968e9ec76ddafb455edffc1c
santani423 <santani423@gmail.com>
2025-07-27 21:51:20 +0700

    menyusaikan gamabr

--- resources/views/siswa/ujian/merge-ujian-show.blade.php
+++ resources/views/siswa/ujian/merge-ujian-show.blade.php
@@ -46,7 +46,7 @@
                                                 style="word-wrap: break-word">
 
                                                 <img src="{{ url($soal->detailujian->soal) }}" alt=""
-                                                    style="width: auto; height: 80vh;">
+                                                    style="width: 70Wh; height: 30vh;">
                                             </h6>
                                         </div>
 
