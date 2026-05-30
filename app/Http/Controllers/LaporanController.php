e9c8eaeaf95e87735f51807a533b98fb22f950b3
santani423 <santani423@gmail.com>
2026-05-30 23:07:42 +0700

    ok

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Guru;
 use Illuminate\Http\Request;
 
 class LaporanController extends Controller
@@ -9,6 +10,7 @@ class LaporanController extends Controller
     public function laporan_ujian_siswa(Request $request)
     {
         return view('guru.laporan.laporan_ujian_siswa', [
+            'guru' => Guru::firstWhere('id', session()->get('id')),
             'title' => 'Data Tes',
             'plugin' => '
                 <link rel="stylesheet" type="text/css" href="' . url("/assets/cbt-malela") . '/plugins/table/datatable/datatables.css">
